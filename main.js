a4107e2531e3ce62f04d1d99fedee9b4bbf66be2
NullVoxPopuli <LPSego3+dev@gmail.com>
2020-02-29 11:11:27 -0500

    support pre-built-assets, too

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import { getInput, setFailed } from '@actions/core';
 import { exec } from '@actions/exec';
 import { GitHub, context } from '@actions/github';
+import path from 'path';
 
 import {
   normaliseFingerprint,
@@ -10,42 +11,65 @@ import {
   getAssetSizes,
 } from './lib/helpers';
 
-let octokit;
-
 async function run() {
   try {
-    const myToken = getInput('repo-token', { required: true });
-    octokit = new GitHub(myToken);
+    const { myToken, cwd, usePrArtifacts } = getActionInputs();
+
+    const octokit = new GitHub(myToken);
+
     const pullRequest = await getPullRequest(context, octokit);
+    const fileDiffs = await diffAssets({ pullRequest, cwd, usePrArtifacts });
 
-    const prAssets = await getAssetSizes();
+    await commentOnPR({ octokit, pullRequest, fileDiffs })
+  } catch (error) {
+    setFailed(error.message);
+  }
+}
 
-    await exec(`git checkout ${pullRequest.base.sha}`);
+async function getActionInputs() {
+  const workingDirectory = getInput('working-directory', { required: false });
+  const usePrArtifacts = getInput('use-pr-artifacts', { required: false });
+  const myToken = getInput('repo-token', { required: true });
 
-    const masterAssets = await getAssetSizes();
+  const cwd = path.join(process.cwd(), workingDirectory);
 
-    const fileDiffs = diffSizes(normaliseFingerprint(masterAssets), normaliseFingerprint(prAssets));
+  return { myToken, cwd, usePrArtifacts };
+}
 
-    const body = buildOutputText(fileDiffs);
+async function diffAssets({ pullRequest, cwd, usePrArtifacts }) {
+  const prAssets = await getAssetSizes({ cwd, build: !usePrArtifacts });
 
-    try {
-      await octokit.issues.createComment({
-        owner: context.repo.owner,
-        repo: context.repo.repo,
-        issue_number: pullRequest.number,
-        body,
-      });
-    } catch (e) {
-      console.log(`Could not create a comment automatically. This could be because github does not allow writing from actions on a fork.
+  await exec(`git checkout ${pullRequest.base.sha}`, { cwd });
+
+  const masterAssets = await getAssetSizes({ cwd, build: true });
+
+  const fileDiffs = diffSizes(
+    normaliseFingerprint(masterAssets),
+    normaliseFingerprint(prAssets)
+  );
+
+
+  return fileDiffs;
+}
+
+async function commentOnPR({ octokit, pullRequest, fileDiffs }) {
+  const body = buildOutputText(fileDiffs);
+
+  try {
+    await octokit.issues.createComment({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      issue_number: pullRequest.number,
+      body,
+    });
+  } catch (e) {
+    console.log(`Could not create a comment automatically. This could be because github does not allow writing from actions on a fork.
 
 See https://github.community/t5/GitHub-Actions/Actions-not-working-correctly-for-forks/td-p/35545 for more information.`);
 
-      console.log(`Copy and paste the following into a comment yourself if you want to still show the diff:
+    console.log(`Copy and paste the following into a comment yourself if you want to still show the diff:
 
 ${body}`);
-    }
-  } catch (error) {
-    setFailed(error.message);
   }
 }
 
